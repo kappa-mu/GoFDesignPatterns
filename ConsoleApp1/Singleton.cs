de4d3f8987cf5659fa3adf8571267eae1f208398
krishanu.tito@gmail.com <krishanu.tito@gmail.com>
2021-08-05 21:07:24 +0530

    Add project files.

---
+++ ConsoleApp1/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        static Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get { return _instance;  }
+        }
+    }
+}
